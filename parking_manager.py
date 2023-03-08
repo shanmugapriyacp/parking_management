c2fcd87f926df76a59b5247ad7ad65916160fef0
shanmugapriyacp <43047666+shanmugapriyacp@users.noreply.github.com>
2023-03-08 18:43:28 +0530

    Update parking_manager.py

--- parking_manager.py
+++ parking_manager.py
@@ -1,9 +1,10 @@
+import sys
 import math
 import traceback
 import validictory
 
 from fee_manager import FeeComputationManager
-from parking_config import MALL, STADIUM, AIRPORT, VEHICLE_TYPE_VALIDATION, TICKET_NUMBER_VALIDATION
+from parking_config import MALL, STADIUM, AIRPORT, VEHICLE_TYPE_VALIDATION, MOTOR_CYCLE, CAR_SUV, BUS_TRUCK, TICKET_NUMBER_VALIDATION
 from slot_manager import SlotManager
 
 
@@ -21,9 +22,9 @@ class ParkingManagement:
     def vehicle_entry(self, vehicle_type):
         try:
             validictory.validate(vehicle_type, VEHICLE_TYPE_VALIDATION)
-            return self._slot_manager.assign_slot(vehicle_type)
+            return "Your Ticket is " + str(self._slot_manager.assign_slot(vehicle_type))
         except Exception as e:
-            print(traceback.format_exc())
+            # print(traceback.format_exc())
             return str(e)
 
     def vehicle_exit(self, ticket_number):
@@ -35,9 +36,9 @@ class ParkingManagement:
                 'parked_time': parked_time,
                 'vehicle_type': vehicle_type,
             }
-            return self._fee_model.calculate(computation_data)
+            return "Your Fee amount is " + str(self._fee_model.calculate(computation_data))
         except Exception as e:
-            print(traceback.format_exc())
+            # print(traceback.format_exc())
             return str(e)
 
 
@@ -75,17 +76,22 @@ class MallParking(ParkingManagement):
 
 
 if __name__ == '__main__':
-    ap = AirportParking()
-    t1 = ap.vehicle_entry('car_suv')
-    t2 = ap.vehicle_entry('bus_truck')
-    t3 = ap.vehicle_entry('motor_cycle')
-    t4 = ap.vehicle_entry('car_suv')
+    venue_class_map = {'1': AirportParking,
+                       '2': MallParking,
+                       '3': StadiumParking}
+    parking_type_map = {'1': 'vehicle_entry',
+                        '2': 'vehicle_exit'}
+    vehicle_type_map = {'1': MOTOR_CYCLE,
+                        '2': CAR_SUV,
+                        '3': BUS_TRUCK
+                        }
+    venue = input("Select the venue \n1.%s \n2. %s \n3.%s \n" % (AIRPORT, MALL, STADIUM))
+    parking_type = input("Press \n1. Park \n2. Unpark \n")
+    if parking_type == '1':
+        vehicle_type = input("Select Vehicle \n1.%s \n2.%s \n3.%s \n" % (MOTOR_CYCLE, CAR_SUV, BUS_TRUCK))
+        argu = vehicle_type_map[vehicle_type]
+    elif parking_type == '2':
+        argu = input("Please Enter Your Ticket Number: ")
+        argu = int(argu)
 
-    print(t1, t2, t3, t4)
-    import time
-
-    time.sleep(65)
-    print("fee for t4", ap.vehicle_exit(t1))
-    print("fee for t2", ap.vehicle_exit(t2))
-    print("fee for t3", ap.vehicle_exit(t3))
-    print("fee for t1", ap.vehicle_exit(t4))
+    print(getattr(venue_class_map[venue](), parking_type_map[parking_type])(argu))
